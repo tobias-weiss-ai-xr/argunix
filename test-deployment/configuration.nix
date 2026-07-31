05abeb9f7bd824d2b04974fee62ec2db3e865d4f
Jacek Galowicz <jacek@applicative.systems>
2026-07-31 12:57:14 +0200

    add another key to testdeployment

--- test-deployment/configuration.nix
+++ test-deployment/configuration.nix
@@ -56,6 +56,7 @@ in
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH6Z4dj1RU+44lXXW1Dw6TW4cLtV/4+qJRO7vFOmyC6C tfc@ai"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7I8SFIFoKEBJEPEEUp14PuwA1Z+olKcL3OKlaxI//6 tfc@framejonge"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF0iyQuS7BycsHPZCLpkl0ojyiRn2GCPwNhtFjdnwHZx jacek@galowicz.de"
   ];
 
   services.argunix = {
